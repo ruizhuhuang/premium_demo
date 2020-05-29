261fd87670d51826dabce058aec8a1cb4298a917
ruizhuhuang <ruizhuhuang@gmail.com>
2020-05-29 11:00:33 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 CUR_DIR=`pwd`
-WORKING_DIR='/Users/rhuang/Documents/Dropbox_1/TACC/premium/demo'
+WORKING_DIR='/home/tacc/premium_demo'
 cd $WORKING_DIR
 Rscript test_full.R $1
 cp summary_1_disc_9_cont_3.2.3.png $CUR_DIR/public/images/summary_1_disc_9_cont_3.2.3.png
